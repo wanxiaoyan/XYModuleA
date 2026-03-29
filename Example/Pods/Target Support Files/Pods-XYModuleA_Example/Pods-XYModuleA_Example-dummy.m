0f3c440c08ee74ebfabdb686252b162a43f88cc7
wanxiaodi3 <wanxiaodi3@jd.com>
2026-03-29 17:09:21 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-XYModuleA_Example/Pods-XYModuleA_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XYModuleA_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XYModuleA_Example
+@end
---
+++ Example/Pods/Target Support Files/XYModuleA/XYModuleA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XYModuleA : NSObject
+@end
+@implementation PodsDummy_XYModuleA
+@end
